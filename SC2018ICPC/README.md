b953b05b6af407f781e58dfe7072af9fe2c1b8ed
lordbecerril <becerrilblas@gmail.com>
2020-02-21 03:42:11 -0800

    Finished problem 9 from 2018

--- SC2018ICPC/README.md
+++ SC2018ICPC/README.md
@@ -1,27 +1,26 @@
 # 2018 Problem Set
 
-:squirrel: Means the problem is complete <br />
-:neckbeard: Means the problem is not complete <br />
+:neckbeard: Means the problem is complete <br />
 
-### Problem 1: Backup Orphans :neckbeard:
+### Problem 1: Backup Orphans  
 
-### Problem 2: Lost is Close to Lose :neckbeard:
+### Problem 2: Lost is Close to Lose  
 
-### Problem 3: Collusion on Two Wheels :neckbeard:
+### Problem 3: Collusion on Two Wheels  
 
-### Problem 4: Floor Tiling :neckbeard:
+### Problem 4: Floor Tiling  
 
-### Problem 5: Playing the Slots :neckbeard:
+### Problem 5: Playing the Slots  
 
-### Problem 6: All Your Base :neckbeard:
+### Problem 6: All Your Base  
 
-### Problem 7: Find Poly :neckbeard:
+### Problem 7: Find Poly  
 
-### Problem 8: Capacity Problem :neckbeard:
+### Problem 8: Capacity Problem  
 
-### Problem 9: Toll Roads :neckbeard:
+### Problem 9: Toll Roads  :neckbeard:
 TollRoads.py is solution in python<br />
 input9.txt is the corresponding input file <br />
-### Problem 10: ABC Conjecture :neckbeard:
+### Problem 10: ABC Conjecture  
 
-### Problem 11: Picking Up the Dice :neckbeard:
+### Problem 11: Picking Up the Dice  
--- SC2018ICPC/TollRoads.py
+++ SC2018ICPC/TollRoads.py
@@ -1,14 +1,42 @@
 ######################
-# Author: Eric Becerril-Blas, Zoyla Orellana, Itzel Becerril
+# Author: Eric Becerril-Blas
 # Main Idea: If you have taken statistics, this is the rule of multiplication
+# Run in command line with: python TollRoads.py < input9.txt
+# Key things,  Dict, Hash
 ######################
 
-# Bottom line is for file redirection
 import sys
 
-# Create Hash Table and pre-populate it all with 1
-
-
-# Read through the file input
+# Read through the file input and grab statistics
+Stat_vals = []
+licence_plates = []
+flag = False
 for line in sys.stdin:
     print (line)
+    line = line.strip()
+    if line == "":
+        flag = True
+    if line and flag == False:
+        Stat_vals.append(line)
+    if flag == True:
+        licence_plates.append(line)
+
+#print(Stat_vals)
+first_line = Stat_vals[0].split()
+
+this_dict = {}
+for n in range(len(Stat_vals)):
+    line = Stat_vals[n].split()
+    this_dict.update({line[0]:line[1]})
+#print(this_dict)
+
+for plate in licence_plates:
+    prob = 1
+    #print(plate)
+    for element in plate:
+        #print(element)
+        if element in this_dict.keys():
+            prob = prob * float(this_dict.get(element, ""))
+        else:
+            prob = prob * 1
+    print(prob)
